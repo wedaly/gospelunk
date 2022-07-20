6327b9a7679d8c39d071b9c67330bebdc312fc07
Will Daly <will.e.daly@gmail.com>
2022-07-19 21:36:00 -0700

    wip

--- parsetest/main.go
+++ parsetest/main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/wedaly/gospelunk/pkgmeta"
 )
 
+const NUM_WORKERS = 16
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Printf("USAGE: %s DIR\n", os.Args[0])
@@ -16,29 +18,40 @@ func main() {
 
 	root := os.Args[1]
 
+	var mu sync.Mutex
 	var fileCount, defCount int
+	var wg sync.WaitGroup
 	filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
 
 		if filepath.Ext(path) == ".go" {
-			f, err := os.Open(path)
-			if err != nil {
-				return err
-			}
-			defer f.Close()
+			wg.Add(1)
+			go func(path string) {
+				defer wg.Done()
 
-			godefs, err := pkgmeta.LoadGoDefs(f)
-			if err != nil {
-				return err
-			}
+				f, err := os.Open(path)
+				if err != nil {
+					return err
+				}
+				defer f.Close()
 
-			fileCount++
-			defCount += len(godefs)
+				godefs, err := pkgmeta.LoadGoDefs(f)
+				if err != nil {
+					return err
+				}
+
+				mu.Lock()
+				fileCount++
+				defCount += len(godefs)
+				mu.Unlock()
+
+			}(path)
 		}
 		return nil
 	})
+	wg.Wait()
 
 	fmt.Printf("Parsed %d files with %d defs", fileCount, defCount)
 }
