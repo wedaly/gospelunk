2ef5c25be74c2c3f1787a82473d787646f79f7d3
Will Daly <will.e.daly@gmail.com>
2022-09-12 07:10:58 -0700

    Format template test

--- pkg/output/template_test.go
+++ pkg/output/template_test.go
@@ -6,8 +6,8 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestTemplate(t *testing.T) {
