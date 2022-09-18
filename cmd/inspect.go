fa2c9def20797b2770f42f3c54ecf507fa93d261
Will Daly <will.e.daly@gmail.com>
2022-09-18 11:14:41 -0700

    Rename inspect CLI arg variables

--- cmd/inspect.go
+++ cmd/inspect.go
@@ -12,10 +12,10 @@ import (
 )
 
 var (
-	FileArg     string
-	LineArg     int
-	ColumnArg   int
-	TemplateArg string
+	InspectFileArg     string
+	InspectLineArg     int
+	InspectColumnArg   int
+	InspectTemplateArg string
 )
 
 var inspectCmd = &cobra.Command{
@@ -23,19 +23,19 @@ var inspectCmd = &cobra.Command{
 	Short: "inspect Go code",
 	Long:  "inspect type information and definition location for an identifier in a Go source file",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		tmpl, err := output.Template(TemplateArg)
+		tmpl, err := output.Template(InspectTemplateArg)
 		if err != nil {
 			return err
 		}
 
-		loc := inspect.FileLoc{Path: FileArg, Line: LineArg, Column: ColumnArg}
+		loc := inspect.FileLoc{Path: InspectFileArg, Line: InspectLineArg, Column: InspectColumnArg}
 		result, err := inspect.Inspect(loc)
 		if err != nil {
 			return err
 		}
 
 		if result == nil {
-			fmt.Fprintf(os.Stderr, "No results found for %s:%d:%d\n", FileArg, LineArg, ColumnArg)
+			fmt.Fprintf(os.Stderr, "No results found for %s:%d:%d\n", InspectFileArg, InspectLineArg, InspectColumnArg)
 			return nil
 		}
 
@@ -49,16 +49,16 @@ var inspectCmd = &cobra.Command{
 }
 
 func init() {
-	inspectCmd.Flags().StringVarP(&FileArg, "file", "f", "", "Go source file")
+	inspectCmd.Flags().StringVarP(&InspectFileArg, "file", "f", "", "Go source file")
 	inspectCmd.MarkFlagRequired("file")
 
-	inspectCmd.Flags().IntVarP(&LineArg, "line", "l", 1, "Line number in Go source file")
+	inspectCmd.Flags().IntVarP(&InspectLineArg, "line", "l", 1, "Line number in Go source file")
 	inspectCmd.MarkFlagRequired("line")
 
-	inspectCmd.Flags().IntVarP(&ColumnArg, "column", "c", 1, "Column number in Go source file")
+	inspectCmd.Flags().IntVarP(&InspectColumnArg, "column", "c", 1, "Column number in Go source file")
 	inspectCmd.MarkFlagRequired("column")
 
-	inspectCmd.Flags().StringVarP(&TemplateArg, "template", "t", "{{.Def.Name}} {{.Def.Path|RelPath}}:{{.Def.Line}}:{{.Def.Column}}\n", "Go template for formatting result output")
+	inspectCmd.Flags().StringVarP(&InspectTemplateArg, "template", "t", "{{.Def.Name}} {{.Def.Path|RelPath}}:{{.Def.Line}}:{{.Def.Column}}\n", "Go template for formatting result output")
 
 	rootCmd.AddCommand(inspectCmd)
 }
