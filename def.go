6f90e4c3307cd1b33474c1e0c25caae72c5470f1
Will Daly <will.e.daly@gmail.com>
2022-12-07 06:53:57 -0800

    wip

--- def.go
+++ def.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"go/ast"
 	"os"
 	"path/filepath"
 	"strconv"
+
+	"golang.org/x/tools/go/packages"
 )
 
 func main() {
@@ -34,18 +37,68 @@ func main() {
 }
 
 func lookupGoDef(path string, line int, col int) error {
-	// Step 2: load the Go package.
-	absPath, err := filepath.Abs(pathArg)
+	// Step 1: load the Go package.
+	absPath, err := filepath.Abs(path)
 	if err != nil {
 		return err
 	}
 
+	// TODO strip this down...
+	loadMode := (packages.NeedName |
+		packages.NeedFiles |
+		packages.NeedSyntax |
+		packages.NeedDeps |
+		packages.NeedTypes |
+		packages.NeedTypesInfo)
+
+	cfg := &packages.Config{
+		Mode: loadMode,
+		Dir:  filepath.Dir(absPath),
+	}
+
+	pkgs, err := packages.Load(cfg, ".")
+	if err != nil {
+		return err
+	} else if len(pkgs) == 0 {
+		return fmt.Errorf("No packages loaded")
+	}
+
+	pkg := pkgs[0] // TODO: explain this...
+
+	// Step 2: find the AST for the target path.
+	var astFile *ast.File
+	for _, f := range pkg.Syntax {
+		if pkg.Fset.Position(f.Pos()).Filename == absPath {
+			astFile = f
+			break
+		}
+	}
+	if astFile == nil {
+		return fmt.Errorf("Could not find AST file for %q", absPath)
+	}
+
 	// Step 3: find the AST identifier at the line/column
-	// TODO
+	var astIdent *ast.Ident
+	ast.Inspect(astFile, func(node ast.Node) bool {
+		if node == nil || astIdent != nil {
+			return false
+		}
+		start, end := pkg.Fset.Position(node.Pos()), pkg.Fset.Position(node.End())
+		if line < start.Line || line > end.Line || (line == start.Line && col < start.Column) || (line == end.Line && col > end.Column) {
+			return false
+		}
+		if node, ok := node.(*ast.Ident); ok {
+			astIdent = node
+			return false
+		}
+		return true
+	})
 
 	// Step 4: lookup the definition.
 	// TODO
 
 	// Step 5: print the result.
 	// TODO
+
+	return nil
 }
